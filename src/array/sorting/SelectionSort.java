66f3fe311519d8ef56406c22bb52065fdd614f0d
Amit <amitthecoders@gmail.com>
2025-01-12 12:55:29 +0530

    Complete Selection sort program

--- src/array/sorting/SelectionSort.java
+++ src/array/sorting/SelectionSort.java
@@ -1,35 +1,30 @@
 package array.sorting;
 
+import java.util.Arrays;
+
 class SelectionSort{
     public static void main(String[] args){
         int[] arr = {4,9,-9,0,-1,1,3,6,7};
         int size = arr.length;
 
         for(int i=0;i<size-1;i++){
-            int minimum = arr[i];
-            for(int j=i+1;j<size-1;j++){
+            int minimum = i;
+            for(int j=i+1;j<size;j++){
                 if(arr[j]<arr[minimum]){
-
+                    minimum = j;
                 }
+                    int temp = arr[i];
+                    arr[i] = arr[minimum];
+                    arr[minimum] = temp;
             }
 
         }
 
+        System.out.println(Arrays.toString(arr));
+
 
     }
 }
-
-
-//selectionSort(array, size)
-//  for i from 0 to size - 1 do
-//    set i as the index of the current minimum
-//    for j from i + 1 to size - 1 do
-//        if array[j] < array[current minimum]
-    set j as the new current minimum index
-        if current minimum is not i
-            swap array[i] with array[current minimum]
-end selectionSort
-
 
 /*
 
